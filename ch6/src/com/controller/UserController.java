fb56a0f6629242640c4ad179c7fb240fc9b235b7
LuYao <1281243810@qq.com>
2020-10-28 21:12:54 +0800

    第六章代码实现

--- ch6/src/com/controller/UserController.java
+++ ch6/src/com/controller/UserController.java
@@ -13,13 +13,15 @@ public class UserController {
     private UserDao userDao;
     public void test() {
         //查询一个用户
-        myUser auser = userDao.selectUserById(1);
+        myUser auser = userDao.selectUserById(11);
         System.out.println(auser);
         System.out.println("================");
         //添加一个用户
         myUser addmu = new myUser();
         addmu.setUsername("陈恒");
+        addmu.setPassword("chenheng");
         addmu.setSex("男");
+        addmu.setEmail("123321@ch.com");
         int add = userDao.addUser(addmu);
         System.out.println("添加了" + add + "条记录");
         System.out.println("================");
@@ -32,7 +34,7 @@ public class UserController {
         System.out.println("修改了" + up + "条记录");
         System.out.println( "================");
         //删除一个用户
-        int dl = userDao.deleteUser(9);
+        int dl = userDao.deleteUser(12);
         System.out.println("删除了" + dl + "条记录");
         System.out.println("================");
         //查询所有用户
